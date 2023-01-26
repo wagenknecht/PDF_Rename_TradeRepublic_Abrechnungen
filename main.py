1be828e5e979847a51692747593ef243bc3f798f
wagenknecht <70338036+FWDL@users.noreply.github.com>
2023-01-26 20:41:18 +0100

    robuster gemacht

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import fitz
 from os import DirEntry, curdir, getcwd, chdir, rename
 from glob import glob as glob
 
-directory = 'Abrechnungen'
+directory = 'PDF_FILES'
 chdir(directory)
 
 pdf_list = glob('*.pdf')
@@ -13,12 +13,26 @@ for pdf in pdf_list:
     with fitz.open(pdf) as pdf_obj:
         text = pdf_obj[0].get_text()
         information = list(text.split("\n"))
-        details = information[32].split(' ')
+        for string in information:
+            if string.startswith("Market-Order"):
+                details = string
+                break
+        details = details.split(' ')
 
         # date operations
-        transaction_date = datetime.datetime.strptime(information[11], "%d.%m.%Y")
+        transaction_date = datetime.datetime.strptime(details[3], "%d.%m.%Y,")
+        for i in information:
+            print(i)
+        print(details[5])
         transaction_time = datetime.datetime.strptime(details[5], "%H:%M")
         transaction_isodate = datetime.datetime.combine(transaction_date.date(), transaction_time.time())
 
-        new_file_name = str(transaction_isodate).replace(":", "-") + ' ' + details[1] + ' ' + information[38].replace(".", "")
+        # welche Aktie / ETF
+        for index, string in enumerate(information):
+            if string.startswith("BETRAG"):
+                index_aktie = index + 1
+                break
+
+        new_file_name = str(transaction_isodate).replace(":", "-") + ' ' + details[1] + ' ' + information[index_aktie].replace(".", "")
+        print(new_file_name)
     rename(pdf, new_file_name + '.pdf')
